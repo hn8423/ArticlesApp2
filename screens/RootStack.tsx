8e672f921572f99017c654f89b02b4e061272ef6
김현인 <rolenche@gmail.com>
2022-09-27 09:45:59 +0900

    first commit

---
+++ screens/RootStack.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MainTab from "./MainTab";
+const Stack = createNativeStackNavigator()
+
+function RootStack(){
+  return <Stack.Navigator><Stack.Screen name="MainTab" component={MainTab} options={{headerShown: false}}></Stack.Screen></Stack.Navigator>
+}
+
+export default RootStack
